0f0dd46f89047d50fa1e9b00f2ed22748918f3a0
Christian Duerr <contact@christianduerr.com>
2024-09-27 03:07:28 +0200

    Improve help dialog format
    This moves all the important stuff to the start, making it a little
    easier to grasp things.

--- src/dialog/help.rs
+++ src/dialog/help.rs
@@ -14,19 +14,20 @@ impl HelpDialog {
 impl Dialog for HelpDialog {
     fn lines(&self) -> Vec<String> {
         vec![
-            String::from("MOUSE WHEEL        Change \x1b[32mbrush size\x1b[39m"),
-            String::from("CTRL + LMB         Start \x1b[32mbox drawing\x1b[39m"),
-            String::from("CTRL + DRAG LMB    Assisted \x1b[32mline drawing\x1b[39m"),
-            String::from("CTRL + G           Open brush \x1b[32mgrapheme\x1b[39m dialog"),
-            String::from("CTRL + F           Open \x1b[32mforeground color\x1b[39m dialog"),
-            String::from("CTRL + B           Open \x1b[32mbackground color\x1b[39m dialog"),
-            String::from("CTRL + E           \x1b[32mFill\x1b[39m at brush position"),
-            String::from("CTRL + T           Toggle through \x1b[32mtext styles\x1b[39m"),
-            String::from("CTRL + S           Open \x1b[32msave\x1b[39m dialog"),
-            String::from("CTRL + U           \x1b[32mUndo\x1b[39m last action"),
-            String::from("CTRL + R           \x1b[32mRedo\x1b[39m last undone action"),
-            String::from("CTRL + L           \x1b[32mReset\x1b[39m the canvas"),
-            String::from("CTRL + C           \x1b[32mExit\x1b[39m"),
+            String::from("MOUSE WHEEL        \x1b[32mbrush size\x1b[39m change"),
+            String::from("CTRL + LMB         \x1b[32mbox drawing\x1b[39m mode"),
+            String::from("CTRL + DRAG LMB    \x1b[32mline drawing\x1b[39m mode"),
+            String::from("CTRL + G           \x1b[32mgrapheme\x1b[39m picker"),
+            String::from("CTRL + F           \x1b[32mforeground color\x1b[39m picker"),
+            String::from("CTRL + B           \x1b[32mbackground color\x1b[39m picker"),
+            String::from("CTRL + E           \x1b[32mfill\x1b[39m at brush position"),
+            String::from("CTRL + T           \x1b[32mtext styles\x1b[39m toggle"),
+            String::from("CTRL + S           \x1b[32msave\x1b[39m sketch"),
+            String::from("CTRL + U           \x1b[32mundo\x1b[39m last action"),
+            String::from("CTRL + R           \x1b[32mredo\x1b[39m last undone action"),
+            String::from("CTRL + L           \x1b[32mreset\x1b[39m the canvas"),
+            String::from("CTRL + C           \x1b[32mexit\x1b[39m"),
+            String::from("ESC                \x1b[32mclose\x1b[39m dialog"),
         ]
     }
 }
